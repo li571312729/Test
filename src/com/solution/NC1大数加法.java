c1352bcc12e818621131fad4c14e26a229761dc7
lixiaoqing <lixiaoqing@hefujituan.com>
2021-07-23 14:59:32 +0800

    更新算法

---
+++ src/com/solution/NC1大数加法.java
@@ -0,0 +1,62 @@
+package com.solution;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author lxq
+ * @date 2021年07月22日 10:13
+ */
+public class NC1大数加法 {
+
+    public static void main(String[] args) {
+        System.out.println(solve("1", "99"));
+    }
+
+    /**
+     * 描述
+     * 以字符串的形式读入两个数字，编写一个函数计算它们的和，以字符串形式返回。
+     * （字符串长度不大于100000，保证字符串仅由'0'~'9'这10种字符组成）
+     * @author lxq
+     * @date 2021/7/22 10:14
+     * @param s
+     * @param t
+     * @return java.lang.String
+     */
+    public static String solve (String s, String t) {
+        Deque<Integer> stack1 = new ArrayDeque<>();
+        Deque<Integer> stack2 = new ArrayDeque<>();
+
+        char[] s1 = s.toCharArray();
+        char[] t1 = t.toCharArray();
+
+        for (int i = s1.length - 1, j = t1.length - 1; i >= 0 || j >= 0; i--, j--) {
+            if (i >= 0) {
+                stack1.offer((int)s1[i] - 48);
+            }else {
+                stack1.offer(0);
+            }
+
+            if (j >= 0) {
+                stack2.offer((int)t1[j] - 48);
+            }else {
+                stack2.offer(0);
+            }
+        }
+
+        int data = 0;
+        int carry = 0;
+        StringBuffer result = new StringBuffer();
+        while (stack1.size() > 0){
+            data = stack1.poll() + stack2.poll() + carry;
+            carry = data / 10;
+            result.insert(0, data % 10);
+        }
+
+        if(carry > 0){
+            result.insert(0, carry);
+        }
+        
+        return result.toString();
+    }
+}
--- src/com/solution/判断链表是否有环.java
+++ src/com/solution/判断链表是否有环.java
@@ -7,6 +7,8 @@ package com.solution;
 public class 判断链表是否有环 {
 
     public static void main(String[] args) {
+
+
     }
 
     /**
@@ -37,6 +39,41 @@ public class 判断链表是否有环 {
             }
         }
         return false;
+    }
+
+    /**
+     * 判断给定的链表中是否有环。如果有环则返回true，否则返回false。
+     * @author lxq
+     * @date 2021/6/9 10:34
+     * @param pHead
+     * @return boolean
+     */
+    public ListNode EntryNodeOfLoop(ListNode pHead) {
+        if(pHead == null){
+            return null;
+        }
+
+        ListNode fast = pHead;
+        ListNode slow = pHead;
+
+        while (fast != null){
+            slow = slow.next;
+            if(fast.next != null){
+                fast = fast.next.next;
+            }else {
+                return null;
+            }
+
+            if(fast == slow){
+                fast = pHead;
+                while (fast != slow){
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return fast;
+            }
+        }
+        return null;
     }
 }
 
