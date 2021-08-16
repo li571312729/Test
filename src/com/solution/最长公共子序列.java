44b3bb7c13ce1b5a9a9c5557324f42d404fb1b15
BaiLi <571312729@qq.com>
2021-08-16 20:03:20 +0800

    动态规划 NC59矩阵的最小路径和  牛客 kuanglan

---
+++ src/com/solution/最长公共子序列.java
@@ -0,0 +1,29 @@
+package com.solution;
+
+public class 最长公共子序列 {
+
+    //求解str1 和 str2 的最长公共子序列
+    public static int LCS(String str1, String str2){
+        int[][] c = new int[str1.length() + 1][str2.length() + 1];
+
+        for(int i = 1; i <= str1.length(); i++){
+            for(int j = 1; j <= str2.length(); j++)
+            {
+                if(str1.charAt(i-1) == str2.charAt(j-1)){
+                    c[i][j] = c[i-1][j-1] + 1;
+                }else{
+                    c[i][j] = Math.max(c[i][j-1] , c[i-1][j]);
+                }
+            }
+        }
+        return c[str1.length()][str2.length()];
+    }
+
+    //test
+    public static void main(String[] args) {
+        String str1 = "BDCABA";
+        String str2 = "ABCBDAB";
+        int result = LCS(str1, str2);
+        System.out.println(result);
+    }
+}
