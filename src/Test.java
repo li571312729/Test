c74113aac2b06d0ba9f58acc3ef5ff2571e9a266
lixiaoqing <lixiaoqing@hefujituan.com>
2021-09-03 17:28:56 +0800

    沉没岛屿

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+import java.io.PrintStream;
+import java.lang.invoke.MethodType;
+
+/**
+ * @author lxq
+ * @date 2021年09月01日 17:47
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
