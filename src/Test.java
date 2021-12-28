78703253addef13a770bb7ba3e6d505ac1093576
xiaoqiangli <xiaoqiangli@trip.com>
2021-12-28 16:19:18 +0800

    划分字母区间

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,10 @@
 import java.io.PrintStream;
 import java.lang.invoke.MethodType;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * @author lxq
@@ -7,10 +12,25 @@ import java.lang.invoke.MethodType;
  */
 public class Test {
 
+    private int age;
+
     public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list = list.stream().filter(o -> o > 2).collect(Collectors.toList());
+        Integer first = list.stream().distinct().findFirst().orElse(null);
+        System.out.println(first);
+    }
 
+    public Test(int age) {
+        this.age = age;
+    }
 
+    public int getAge() {
+        return age;
+    }
 
-
+    public void setAge(int age) {
+        this.age = age;
     }
 }
