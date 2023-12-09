947db0ffdaa32b4db53d31e34f2b9e9941b9fd51
li571312729 <571312729@qq.com>
2023-12-09 19:15:58 +0800

    test

--- src/com/algorithm/Test1.java
+++ src/com/algorithm/Test1.java
@@ -1,15 +1,33 @@
 package com.algorithm;
 
-import com.ibm.icu.impl.Assert;
-
-import java.util.ArrayList;
-import java.util.Base64;
+import com.jacob.activeX.ActiveXComponent;
+import com.jacob.com.Dispatch;
+import com.jacob.com.Variant;
 
 public class Test1 {
     public static void main(String[] args) {
-        String a = "-12";
-        int i = Integer.parseInt(a);
-        String s = String.valueOf(i);
-        System.out.println(s);
+        String text = "利刃在手制裁八方!"; // 要转换为语音的文本
+
+        // 创建 ActiveX 组件
+        ActiveXComponent speak = new ActiveXComponent("Sapi.SpVoice");
+
+        try {
+            // 音量 0 - 100
+            speak.setProperty("Volume", new Variant(100));
+            // 语音朗读速度 -10 到 +10
+            speak.setProperty("Rate", new Variant(0));
+
+            // 获取 Dispatch 对象
+            Dispatch dispatch = speak.getObject();
+
+            // 设置语音属性
+            Dispatch.call(dispatch, "Speak", new Variant(text));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 释放资源
+            speak.safeRelease();
+        }
     }
 }
